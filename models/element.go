c30117acf5048c09956ec4fb9b1716e58d8d7cdd
Harun Uz <36775062+blackkk466@users.noreply.github.com>
2018-07-05 14:30:57 +0200

    Delete element.go

--- models/element.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Element struct {
-	Title         string        `json:"title,omitempty"`
-	Subtitle      string        `json:"subtitle,omitempty"`
-	ImageURL      string        `json:"image_url,omitempty"`
-	DefaultAction DefaultAction `json:"default_action,omitempty"`
-	Buttons       []Button      `json:"buttons,omitempty"`
-}

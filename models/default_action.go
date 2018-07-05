f7cf75fb004b1038cab1c7a9ce0e98b1c7f250f2
Harun Uz <36775062+blackkk466@users.noreply.github.com>
2018-07-05 14:30:48 +0200

    Delete default_action.go

--- models/default_action.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type DefaultAction struct {
-	Type                string `json:"type,omitempty"`
-	URL                 string `json:"url,omitempty"`
-	MessangerExtensions bool   `json:"messenger_extensions,omitempty"`
-	WebviewHeightRatio  string `json:"webview_height_ratio,omitempty"`
-	FallbackURL         string `json:"fallback_url,omitempty"`
-}

7f286d239ec6099a9382f865cc85ca6c74ac9e98
Harun Uz <36775062+blackkk466@users.noreply.github.com>
2018-07-05 14:31:28 +0200

    Delete messaging.go

--- models/messaging.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Messaging struct {
-	Sender    User    `json:"sender,omitempty"`
-	Recipient User    `json:"recipient,omitempty"`
-	Timestamp int     `json:"timestamp,omitempty"`
-	Message   Message `json:"message,omitempty"`
-}

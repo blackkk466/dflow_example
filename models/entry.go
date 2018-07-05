b099fe9a5884613f8e12531bc602724e505255fb
Harun Uz <36775062+blackkk466@users.noreply.github.com>
2018-07-05 14:31:06 +0200

    Delete entry.go

--- models/entry.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Entry struct {
-	ID        string      `json:"id,omitempty"`
-	Time      int         `json:"time,omitempty"`
-	Messaging []Messaging `json:"messaging,omitempty"`
-}
